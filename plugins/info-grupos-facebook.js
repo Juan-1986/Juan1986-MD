4fc624e034df3bdcc65cbdda28774f177689bdb8
Juan-1986 <juan_saiyan1986@hotmail.com>
2025-02-09 19:51:41 +0100

    Add files via upload

---
+++ plugins/info-grupos-facebook.js
@@ -0,0 +1,109 @@
+let handler = async (m, { conn, command }) => {
+let media = facebook
+let str = `𝗕𝘂𝘀𝗰𝗮𝘀 𝗴𝗿𝘂𝗽𝗼𝘀 𝗱𝗲 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗮𝗾𝘂𝗶 𝘁𝗲 𝗱𝗲𝗷𝗮𝗺𝗼𝘀 𝘂𝗻𝗼𝘀 𝗴𝗿𝘂𝗽𝗼𝘀 𝗿𝗲𝗰𝗼𝗺𝗲𝗻𝗱𝗮𝗱𝗼𝘀 😁
+
+*➤ 🄶🅁🅄🄿🄾🅂 🄵🄰🄲🄴🄱🄾🄾🄺*
+
+𝗙𝗮𝗻𝘀 𝗱𝗲 𝗱𝗿𝗮𝗴𝗼𝗻 𝗯𝗮𝗹𝗹 𝗭 𝗞𝗮𝗶 🇪🇸
+*${grupo_facebook1}*
+
+𝗔𝗹𝗶𝗲𝗻 𝗩𝗦 𝗱𝗲𝗽𝗿𝗲𝗱𝗮𝗱𝗼𝗿  🇪🇸
+*${grupo_facebook2}*
+
+𝗦𝘁𝗮𝗿𝘁 𝘁𝗿𝗲𝗸 𝘄𝗮𝗿𝘀 🇪🇸
+*${grupo_facebook3}*
+
+𝗟𝗼𝘀 𝗰𝗮𝗯𝗮𝗹𝗹𝗲𝗿𝗼𝘀 𝗱𝗲𝗹 𝘇𝗼𝗱𝗶𝗮𝗰𝗼 🇪🇸
+*${grupo_facebook4}*
+
+𝗘𝗹 𝘁𝗮𝗸𝗲 𝗱𝗼𝗯𝗹𝗮𝗷𝗲 🇪🇸
+*${grupo_facebook5}*
+
+𝗙𝗮𝗻𝘀 𝗱𝗲 𝗗𝗿𝗮𝗴𝗼𝗻 𝗕𝗮𝗹𝗹 𝗞𝗮𝗸𝘂𝗺𝗲𝗶
+*${grupo_facebook6}*
+
+𝗡𝗼𝘀𝘁𝗮𝗹𝗳𝗮𝗻 𝗿𝗲𝘁𝗿𝗼 𝟴𝟬 𝘆 𝟵𝟬
+*${grupo_facebook7}*
+
+𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝕃𝕖𝕘𝕖𝕟𝕕𝕤
+*${grupo_facebook8}*
+
+𝗩𝘂𝗲𝘀𝘁𝗿𝗼 𝗴𝗿𝘂𝗽𝗼 𝗱𝗲 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽
+*${grupo_facebook9}*
+
+𝗠𝗶𝘀 𝗴𝗿𝘂𝗽𝗼𝘀 𝗱𝗲 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽
+*${grupo_facebook10}*
+
+𝗢𝗹𝗶𝘃𝗲𝗿 𝘆 𝗕𝗲𝗻𝗷𝗶
+*${grupo_facebook11}*
+
+𝗚𝗿𝘂𝗽𝗼𝘀 𝗱𝗲 𝗱𝗿𝗮𝗴𝗼𝗻 𝗯𝗮𝗹𝗹 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽
+*${grupo_facebook12}*
+
+𝗔𝗻𝗶𝗺𝗲 𝗱𝗲 𝗹𝗼𝘀 𝟴𝟬,𝟵𝟬 🇪🇸
+*${grupo_facebook13}*
+
+𝗔𝗺𝗶𝗴𝗼𝘀 𝗱𝗲𝗹 𝗱𝗼𝗯𝗹𝗮𝗷𝗲 𝗱𝗲 𝗹𝗮 𝗰𝗼𝗺𝘂𝗻𝗶𝗱𝗮𝗱 𝗲𝗻 𝗴𝗲𝗻𝗲𝗿𝗮𝗹
+*${grupo_facebook14}*
+
+🐉🐲亀𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝕕𝕚𝕓𝕦𝕛𝕠𝕤亀🐲🐉
+*${grupo_facebook15}*
+
+𝗙𝗮𝗻𝘀 𝗱𝗲 𝗱𝗿𝗮𝗴𝗼𝗻 𝗯𝗮𝗹𝗹 𝗭 𝗞𝗮𝗶
+*${grupo_facebook16}*
+
+𝗙𝗮𝗻𝘀 𝗱𝗲 𝗹𝗮 𝗯𝗼𝗹𝗮 𝗱𝗲 𝗱𝗿𝗮𝗰 𝗲𝗻 𝗰𝗮𝘁𝗮𝗹𝗮
+*${grupo_facebook17}*
+
+𝗙𝗮𝗻𝘀 𝗱𝗲 𝗹𝗮 𝗯𝗼𝗹𝗮 𝗱𝗲 𝗱𝗿𝗮𝗰 𝗲𝗻 𝗩𝗮𝗹𝗲𝗻𝗰𝗶𝗮
+*${grupo_facebook18}*
+
+𝗗𝗿𝗮𝗴𝗼𝗻 𝗕𝗮𝗹𝗹 𝗗𝗮𝗶𝗺𝗮
+*${grupo_facebook19}*
+
+𝗙𝗮𝗻𝘀 𝗱𝗲 𝗗𝗿𝗮𝗴𝗼𝗺 𝗕𝗮𝗹𝗹 𝗔𝗙
+*${grupo_facebook20}*
+
+𝗙𝗮𝗻𝘀 𝗱𝗲 𝗦𝘂𝗽𝗲𝗿 𝗗𝗿𝗮𝗴𝗼𝗻 𝗕𝗮𝗹𝗹 𝗛𝗲𝗿𝗼𝗲𝘀
+*${grupo_facebook21}*
+
+𝗦𝘁𝗮𝗿 𝗧𝗿𝗲𝗸 𝗪𝗮𝗿𝘀 𝗰𝗼𝗺𝘂𝗻𝗶𝗱𝗮𝗱 𝗳𝗮𝗻𝘀
+*${grupo_facebook22}*
+
+𝗣𝗲𝗹𝗶𝗰𝘂𝗹𝗮𝘀 𝘆 𝘀𝗲𝗿𝗶𝗲𝘀 𝗱𝗲 𝗰𝗶𝗲𝗻𝗰𝗶𝗮 𝗳𝗶𝗰𝗰𝗶𝗼𝗻 𝘆 𝘁𝗲𝗿𝗿𝗼𝗿
+*${grupo_facebook23}*
+
+𝗗𝗖 𝗩𝗦 𝗠𝗮𝗿𝘃𝗲𝗹 𝗰𝗼𝗺𝘂𝗻𝗶𝗱𝗮𝗱 𝗱𝗲 𝗳𝗮𝗻𝘀
+*${grupo_facebook24}*
+
+𝗔𝗺𝗶𝗴𝗼𝘀 𝗱𝗲𝗹 𝗱𝗼𝗯𝗹𝗮𝗷𝗲 𝗱𝗲 𝗹𝗮 𝗰𝗼𝗺𝘂𝗻𝗶𝗱𝗮𝗱 𝗲𝗻 𝗴𝗲𝗻𝗲𝗿𝗮𝗹
+*${grupo_facebook25}*
+
+𝗠𝗮𝘀 𝗴𝗿𝘂𝗽𝗼𝘀 𝗲𝗻 𝗼𝘁𝗿𝗮𝘀 𝗽𝗹𝗮𝘁𝗮𝗳𝗼𝗿𝗺𝗮𝘀 𝗽𝗼𝗻𝗴𝗮𝗻 𝗹𝗼 𝘀𝗶𝗴𝘂𝗶𝗲𝗻𝘁𝗲:
+
+.𝗰𝗮𝗻𝗮𝗹𝗲𝘀𝘄𝗵𝗮𝘁𝘀𝗔𝗽𝗽
+.𝗴𝗿𝘂𝗽𝗼𝘀𝗳𝗿𝗶𝗸𝗶𝘀
+.𝗴𝗿𝘂𝗽𝗼𝘀𝘄𝗵𝗮𝘁𝘀𝗮𝗽𝗽
+.𝗰𝗮𝗻𝗮𝗹𝗲𝘀𝘁𝗲𝗹𝗲𝗴𝗿𝗮𝗺
+
+*Actualizado 01/02/25.*
+
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+*Ayudanos con el gasto del servidor http://paypal.me/GokuBotyJuan1986*
+*Por favor, no ingresar con números de Bots, y mantener el respeto.*\n
+*Please, do not enter with Bot numbers, and maintain respect.*`
+await conn.sendButton(m.chat, str, `🤖 𝐆𝐨𝐤𝐮𝐁𝐨𝐭 🤖🉐\n${asistencia}\n\n` + wm, media, [
+['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentasgb'],
+['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']], null, [
+['🤖 𝐆𝐨𝐤𝐮𝐁𝐨𝐭 🤖🉐', `${md}`]], fkontak)}
+/*conn.sendHydrated(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n${asistencia}\n\n` + wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
+['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentasgb'],
+['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
+['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+], m,)}*/
+
+handler.command = /^gruposfacebook|grupofacebook$/i
+handler.exp = 33
+
+export default handler
